f76a08a39e7cd4bec48a0515be3038be1516103a
x0rc3ll <110277112+x0rc3ll@users.noreply.github.com>
2022-07-30 23:50:20 +1000

    window added
    теперь при запуске открывается окно с картинкой

--- src/ImgShow.java
+++ src/ImgShow.java
@@ -1,6 +1,10 @@
 import org.opencv.core.Core;
+import org.opencv.core.Mat;
+import org.opencv.core.MatOfByte;
+import org.opencv.imgcodecs.Imgcodecs;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class ImgShow extends JFrame {
     static{
@@ -9,6 +13,23 @@ public class ImgShow extends JFrame {
     }
 
     public static void main(String[] args) {
+        JFrame window = new JFrame();
+        JLabel screen = new JLabel();
 
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.setVisible(true);
+
+        Mat img = Imgcodecs.imread("src/butterfly.png");
+
+        //конвертируем img в матрицу
+        MatOfByte buf = new MatOfByte();
+        Imgcodecs.imencode(".png", img, buf);
+
+        ImageIcon ic = new ImageIcon(buf.toArray());
+
+        //создаем контейнер с img и привязываем его к окну
+        screen.setIcon(ic);
+        window.getContentPane().add(screen);
+        window.pack();
     }
 }
