8678cae6cb372b054ad0bc34541c9b9eb5be2ece
x0rc3ll <110277112+x0rc3ll@users.noreply.github.com>
2022-07-30 23:31:29 +1000

    Update ImgShow.java

--- src/ImgShow.java
+++ src/ImgShow.java
@@ -3,5 +3,8 @@ import org.opencv.core.Core;
 import javax.swing.*;
 
 public class ImgShow extends JFrame {
-    static{System.loadLibrary(Core.NATIVE_LIBRARY_NAME);}
+    static{
+        System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
+        System.out.println("Lib version: " + Core.VERSION);
+    }
 }
